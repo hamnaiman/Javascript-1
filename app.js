5c9e00054ec0c4043126fd807fec46de3e1a5f77
hamna iman <hamnajawaid22@gmail.com>
2024-05-11 13:02:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// variable for string
+var username
+var myName="Humna Iman";
+// Declare a variable titled message
+var message;
+// Assign "Hello World" to variable message
+message="Hello World"
+//display message in alert boxaler
+alert(message);
+// second task
+// Students bio data
+var biodata;
+// Declare a variable titled message
+var message;
+//assign "15 years old" to variable message
+message="15 years old"
+//display message in alert boxaler
+alert(message)
+
+// third task
+
+var biodata;
+// Declare a variable titled message
+var message;
+//assign "15 years old" to variable message
+message="Certified Mobile Application Development"
+//display message in alert boxaler
+alert(message);
+
+// fourth task
+var biodata;
+// Declare a variable titled message
+var message;
+//assign "15 years old" to variable message
+message="Pizza <br> pizz <br> piz <br> pi <br>";
+//display message in alert boxaler
+alert(message);
+
+// fifth task
+var email;
+email=" My email address is hamnajawaid@gmail.com";
+alert(email);
+
+// sixth task
+var book;
+book="A smarter way to learn JavaScript"
+alert(book)
+
+// eighth task
+var symbols
+symbols="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(symbols)
+
+// seven task
+var container=document.getElementById("container");
+var paragraph = document.createElement("p");
+paragraph.textContent = "Yah! I can write HTML content through JS";
+container.appendChild(paragraph);
+
+
+
+
